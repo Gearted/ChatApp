619297573e3498632603b00da542f95154928beb
Gearted <63016907+Gearted@users.noreply.github.com>
2022-11-21 14:32:14 +0100

    Initial commit

---
+++ back/src/test/java/com/chat/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
